2dc24dce28ebc4b8892cfbce1ba95adc0b4c75b0
Sahoo Himansu <himansu.sahoo1986@gmail.com>
2019-02-24 17:58:25 +0530

    custom app config file added
    Signed-off-by: Sahoo Himansu <himansu.sahoo1986@gmail.com>

---
+++ application/config/custom_app_config.php
@@ -0,0 +1,16 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Library user type
+|--------------------------------------------------------------------------
+| @himansuS
+| @22Feb2019
+| This defines the type of library member and this is directly 
+| related to rbac_user tables user_type column.
+| When ever we add/update/remove the user_type column data then
+| same should be updated here.
+|
+*/
+$config['LIB_USER_TYPE'] = array('staff'=>'staff','student'=>'student','library_member','developer');
+
